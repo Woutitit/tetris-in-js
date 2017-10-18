2b00d88711bcf8c680eff9717f613b9f32fadd49
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-10-18 17:04:33 +0200

    Add classes and properties for game area component

--- src/js/components/VGameArea.vue
+++ src/js/components/VGameArea.vue
@@ -1,5 +1,40 @@
+<style>
+	.game-area {
+		border: 1px solid #000;
+	}
+
+	.game-area--size-normal {
+		width: 200px;
+		height: 200px;
+	}
+	
+</style>
 <template>
+	<canvas :class="classes" :width="width" :height="height"></canvas>
 </template>
 <script>
+	export default {
+		props: {
+			size: {
+				type: String
+			},
+			width: {
+				type: Number
+			},
+			height: {
+				type: Number
+			}
+		},
+		computed: {
+			classes: function() {
+				return {
+					"game-area": true,
+					"game-area--size-normal": this.size == "normal"
+
+				}
+				
+			}
+		}
+	}
 
 </script>

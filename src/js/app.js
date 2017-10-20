250b7ec49d72e8cf270d45e4842795489a298c55
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-10-20 22:17:20 +0200

    Add basic styling canvas

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,8 @@ import Game from "./game.js";
 import NN from "./neural_network.js";
 import Agent from "./agent.js";
 
-// Import components
-import VGameArea from "./components/VGameArea.vue";
-
 var app = new Vue({
 	el: "#app",
-	components: {
-		"v-game-area": VGameArea
-	},
 	mounted: function() {
 		var game = new Game(document.getElementById("canvasOuterContainer")); //TODO: Add a singleton?
 		game.init();
--- src/js/game.js
+++ src/js/game.js
@@ -10,16 +10,11 @@ function Game(outerContainerEl) {
 	this.canvas = null;
 	this.canvasCtx = null;
 
-	this.dimensions = Game.dimensions;
-}
-
-/**
-* Canvas dimensions.
-* @enum {string}
-*/
-Game.dimensions = {
-	WIDTH: 570,
-	HEIGHT: 320
+	// Canvas dimensions
+	this.dimensions = {
+		WIDTH: 570,
+		HEIGHT: 320
+	}
 }
 
 
@@ -29,8 +24,6 @@ Game.prototype = {
 
 		this.canvas = createCanvas(this.dimensions.WIDTH, this.dimensions.HEIGHT);
 		this.canvasCtx = this.canvas.getContext("2d");
-		this.canvasCtx.fillStyle = "#f7f7f7";
-		this.canvasCtx.fill();
 
 		container.appendChild(this.canvas); //To hold canvas
 		this.outerContainerEl.appendChild(container);

4318e142e75e9ce252ff6d8bc809cd80606d0a2c
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-11-08 13:05:35 +0100

    Make tetrominoes already colorful

--- dist/app.js
+++ dist/app.js
@@ -20771,6 +20771,16 @@ function Grid(colSpan, rowSpan, canvas, celSpan) {
 
 	this.CELL_SPAN = celSpan;
 
+	this.GRID_COLORS = {
+		1: "#000",
+		2: "blue",
+		3: "red",
+		4: "purple",
+		5: "blue",
+		6: "green",
+		7: "yellow"
+	};
+
 	this.playingField = [];
 	this.SPAWN_POS = 3; // Default spawn position for tetromino.
 
@@ -20800,9 +20810,6 @@ Grid.prototype = {
 	create: function (colSpan, rowSpan) {
 		// Note: we add + 2 to the backend grid so that our tetrominoes spawn off canvas before they come in the field.
 		return Array(rowSpan).fill().map(() => Array(colSpan).fill(0));
-
-		// Now also put 1's at the first AND last values of each row
-		// Add 1's to the bottom row aswell for colission.
 	},
 
 	/**
@@ -20816,7 +20823,7 @@ Grid.prototype = {
 	},
 
 	detectRow: function (y) {
-		// We should also ad this to a "row" counter for the UI.
+		// TODO: We should also add this to a "row" counter for the UI.
 		var count = 0;
 		this.playingField[y].forEach(x => {
 			if (x !== 0) {
@@ -20835,6 +20842,13 @@ Grid.prototype = {
 			this.playingField[y][x] = 0;
 			this.undraw(x, y);
 		});
+
+		// Will drop all tetrominoes above cleared line.
+		this.dropTetrominoes(y);
+	},
+
+	dropTetrominoes: function (y) {
+		// Start from cleared row and move up.
 		for (var i = y; i >= 0; i--) {
 			// Special case for top row where we make all the values simply 0.
 			if (i === 0) {
@@ -20849,7 +20863,7 @@ Grid.prototype = {
 					// Also if the block above current row is filled we should undraw and redraw it at the new position.
 					if (this.playingField[i - 1][index] !== 0) {
 						this.undraw(index, i - 1);
-						this.draw(index, i, "#000");
+						this.draw(index, i, this.playingField[i - 1][index]);
 					}
 				});
 			}
@@ -20857,30 +20871,10 @@ Grid.prototype = {
 	},
 
 	/**
- * 
- */
-	validateCell: function (x, y) {
-		if (this.playingField[y] === undefined || this.playingField[y][x] === undefined || this.playingField[y][x] === 1) {
-			return false;
-		}
-	},
-
-	/**
- * Check for new occupied (=landed) cells, update the backend playingfield with them and destroy rows if necesarry.
- *
- */
-	update: function (x, y, value) {
-		//this.playingField = this.create(this.COL_SPAN, this.ROW_SPAN);
-		coordinatesPair.forEach(coordinates => {
-			this.playingField[coordinates[1]][coordinates[0]] = 1;
-		});
-	},
-
-	/**
  * Draw a single set of x, y coordinates and its color. Use this in conjunction with a loop.
  */
-	draw: function (x, y, color) {
-		this.canvasCtx.fillStyle = color;
+	draw: function (x, y, colorValue) {
+		this.canvasCtx.fillStyle = this.GRID_COLORS[colorValue];
 		this.canvasCtx.fillRect(x * this.CELL_SPAN, y * this.CELL_SPAN, this.CELL_SPAN, this.CELL_SPAN);
 	},
 
@@ -20889,47 +20883,6 @@ Grid.prototype = {
  */
 	undraw: function (x, y) {
 		this.canvasCtx.clearRect(x * this.CELL_SPAN, y * this.CELL_SPAN, this.CELL_SPAN, this.CELL_SPAN);
-	},
-
-	undrawRow: function (y) {
-		// Note: x will always be 0 when undrawing a row.
-		this.canvasCtx.clearRect(0, y * this.CELL_SPAN, this.CELL_SPAN * this.COL_SPAN, this.CELL_SPAN);
-	},
-
-	drawRow: function (y) {
-		// Note: x will always be 0 when undrawing a row.
-		this.canvasCtx.fillRect(0, y * this.CELL_SPAN, this.CELL_SPAN * this.COL_SPAN, this.CELL_SPAN);
-	},
-
-	/**
- * Pushes coordinates that also should be occupied to the global occupiedCells object.
- */
-	occupyCells: function (coordinates, color) {
-
-		coordinates.forEach(coordinate => {
-			this.occupiedCells[color].push(coordinate);
-		});
-
-		this.update();
-	},
-
-	/**
- * Removes coordinates that should no longer be occupied from the global occupiedCells object.
- */
-	deoccupyCells: function (coordinates) {
-		for (var i = 0; i < coordinates.length; i++) {
-			this.occupiedCells.purple.pop();
-		}
-	},
-
-	checkFullRows: function () {
-		// Check if a row contains all 1's meaning that line should be removed.
-		// Now we check each row however we know we should only check the rows of the y coordinates of the last 4 coordinates that landed.
-		this.playingField.forEach((row, index) => {
-			if (row.filter(x => x === 1).length === this.COL_SPAN) {
-				this.removeRow(index); // index is the y coordinate that should be removed.
-			}
-		});
 	}
 };
 

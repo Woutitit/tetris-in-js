c208e68d3c84b85d128616c9a42ce9abd65cebf3
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-11-14 21:38:43 +0100

    Implement basic fixed timestep algorithm for game loop

--- dist/app.js
+++ dist/app.js
@@ -68,32 +68,47 @@
 /***/ (function(module, exports) {
 
 (function () {
-	var TICK_LENGTH = 50;
+	var TICK_LENGTH = 50; // Will update at a rate of 20Hz (= 20 updates per second).
 	var UPDATE_RATE = 1000 / TICK_LENGTH;
 
-	var delta; // Records how much realtime the game's behind. Also known as lag.
+	var lastFrameTimeStamp = performance.now();
 
+	var delta = 0; // Records how much realtime the game's behind since last game logic update. Also known as lag.
 
 	/**
  * Main game loop.
  * @param {Number} timeStamp - Holds a timestamp of when this function is called.
  */
 	function main(timeStamp) {
-		requestAnimationFrame(main); // Gets called first to ensure the browser receives the request for the next frame in time.
+		// Gets called first to ensure the browser receives the request for the next frame in time.
+		requestAnimationFrame(main);
+		var lastFrameDuration = timeStamp - lastFrameTimeStamp;
+		lastFrameTimeStamp = timeStamp; // Override lastFrameTimeStamp with current one.
 
+		delta += lastFrameDuration;
+
+		// TODO: Add bail function for when the amount of updates to process (= workload) gets too much.
+		while (delta >= UPDATE_RATE) {
+			update(); // Update game logic.
+			delta -= UPDATE_RATE;
+		}
 	};
 
 	/**
  * Update game logic.
  */
-	function update() {};
+	function update() {
+		// Update game logic.
+	};
 
 	/**
  * Render game graphics.
  */
-	function render() {};
+	function render() {
+		// Render graphics.
+	};
 
-	requestAnimationFrame(main);
+	window.requestAnimationFrame(main);
 })();
 
 /***/ })
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,28 @@
 (function() {
-	var TICK_LENGTH = 50;
+	var TICK_LENGTH = 50; // Will update at a rate of 20Hz (= 20 updates per second).
 	var UPDATE_RATE = 1000 / TICK_LENGTH;
 	
-	var delta; // Records how much realtime the game's behind. Also known as lag.
-	
+	var lastFrameTimeStamp = performance.now();
+
+	var delta = 0; // Records how much realtime the game's behind since last game logic update. Also known as lag.
 
 	/**
 	* Main game loop.
 	* @param {Number} timeStamp - Holds a timestamp of when this function is called.
 	*/
 	function main(timeStamp) {
-		requestAnimationFrame(main); // Gets called first to ensure the browser receives the request for the next frame in time.
+		// Gets called first to ensure the browser receives the request for the next frame in time.
+		requestAnimationFrame(main); 
+		var lastFrameDuration = timeStamp - lastFrameTimeStamp;
+		lastFrameTimeStamp = timeStamp; // Override lastFrameTimeStamp with current one.
 
+		delta += lastFrameDuration;
 
+		// TODO: Add bail function for when the amount of updates to process (= workload) gets too much.
+		while(delta >= UPDATE_RATE) {
+			update(); // Update game logic.
+			delta -= UPDATE_RATE;
+		}
 	};
 
 
@@ -20,7 +30,7 @@
 	* Update game logic.
 	*/
 	function update() {
-
+		// Update game logic.
 	};
 
 
@@ -28,8 +38,8 @@
 	* Render game graphics.
 	*/
 	function render() {
-
+		// Render graphics.
 	};
 
-	requestAnimationFrame(main);
+	window.requestAnimationFrame(main);
 })();
